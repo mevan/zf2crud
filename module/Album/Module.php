b0dc5a83f4f16a6dc32581293921ff5a9eea3db0
mev.box@gmail.com <>
2013-04-30 09:18:31 +0000

    chnged code

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -12,6 +12,25 @@ class Module
   
   // getAutoloaderConfig() and getConfig() methods here
 
+   public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+  
     // Add this method:
     public function getServiceConfig()
     {
@@ -33,23 +52,6 @@ class Module
     }
 }
   
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
+  
 }
   
