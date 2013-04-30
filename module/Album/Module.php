193bf26929a178f7d194daac5b8c2f34241b0399
mev.box@gmail.com <>
2013-04-30 09:16:40 +0000

    added error handling to module.php

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -9,27 +9,8 @@ use Zend\Db\TableGateway\TableGateway;
 
 class Module
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
   
-// getAutoloaderConfig() and getConfig() methods here
+  // getAutoloaderConfig() and getConfig() methods here
 
     // Add this method:
     public function getServiceConfig()
@@ -51,3 +32,24 @@ class Module
         );
     }
 }
+  
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
+  
